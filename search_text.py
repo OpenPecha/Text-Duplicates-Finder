9b931bfd850cb2fe8dd3c99bc3ec0bb21f4ff342
jungtop <tenzintopjor27@gmail.com>
2022-06-15 12:28:22 +0530

    converted to class

--- search_text.py
+++ search_text.py
@@ -1,145 +1,153 @@
-from email.mime import base
 from pathlib import Path
 from fuzzysearch import find_near_matches_in_file
-import time
 import threading
-from openpecha.core.layer import Layer, LayerEnum
-from openpecha.core.pecha import OpenPechaFS
-from openpecha.core.ids import get_work_id
+import random
 from uuid import uuid4
 import yaml
-from os.path import exists
+
+class OpfSearchSequence:
+
+    def __init__(self,search_sequence_path,target_opf_path):
+        self.search_sequence_path = search_sequence_path
+        self.target_path = target_opf_path+"/"+Path(target_opf_path).stem+".opf"
+
+    def get_search_segments(self):
+        text = Path(self.search_sequence_path).read_text(encoding="utf-8")
+        first_seg = text[0:50]
+        last_seg = text[-50:]
+        len_of_text = len(text)
+        mid = int(len_of_text/2)
+        mid_seg = text[mid-25:mid+25]
+        return (first_seg,mid_seg,last_seg)
+
+    def get_target_files(self):
+        bases = list(Path(self.target_path+"/base").iterdir())
+        for base in bases:
+            yield base
+
+    def fuzzy_search(self,target_file,segments): 
+        first,mid,last = segments
+        first_match = self.check_matches(first,target_file)
+        mid_match = self.check_matches(mid,target_file)
+        last_match = self.check_matches(last,target_file)
+
+        """ first_thread = threading.Thread(target=check_matches,args=(first,text))
+        mid_thread = threading.Thread(target=check_matches,args=(mid,text))
+        last_thread = threading.Thread(target=check_matches,args=(last,text))
+        
+        first_thread.start()
+        mid_thread.start()
+        last_thread.start()
+
+        first_thread.join()
+        mid_thread.join()
+        last_thread.join() """
+
+        if first_match and mid_match and last_match:
+            return (first_match,last_match)
 
 
-def get_search_segments(search_path):
-    text = Path(search_path).read_text(encoding="utf-8")
-    first_seg = text[0:50]
-    last_seg = text[-50:]
-    len_of_text = len(text)
-    mid = int(len_of_text/2)
-    mid_seg = text[mid-25:mid+25]
-    return (first_seg,mid_seg,last_seg)
-
-def get_target_files(target_path):
-    bases = list(Path(target_path+"/base").iterdir())
-    for base in bases:
-        yield base
-
-def fuzzy_search(base_file,segments): 
-    first,mid,last = segments
-    first_match = check_matches(first,base_file)
-    mid_match = check_matches(mid,base_file)
-    last_match = check_matches(last,base_file)
-
-    """ first_thread = threading.Thread(target=check_matches,args=(first,text))
-    mid_thread = threading.Thread(target=check_matches,args=(mid,text))
-    last_thread = threading.Thread(target=check_matches,args=(last,text))
-    
-    first_thread.start()
-    mid_thread.start()
-    last_thread.start()
-
-    first_thread.join()
-    mid_thread.join()
-    last_thread.join() """
-
-    if first_match and mid_match and last_match:
-        return (first_match,last_match)
+    def check_matches(self,search_pattern,target_file):
+        with open(target_file.as_posix(), 'r',encoding='utf8') as f:
+            match = find_near_matches_in_file(search_pattern,f,max_l_dist=10)
+        return match
 
 
-def check_matches(search_pattern,target_file):
-    with open(target_file.as_posix(), 'r',encoding='utf8') as f:
-        match = find_near_matches_in_file(search_pattern,f,max_l_dist=10)
-    return match
+    def create_index(self,span,target_file):
+        annotations,spans = self.get_annotations(span,target_file)
+        annotations_yml = self.toyaml(annotations)
+        Path(f"{self.target_path}/index.yml").write_text(annotations_yml)
+        instance = self.add_instances(spans)
+        return instance
 
+        """ opf = OpenPechaFS(path=target_path)
+        annotations,spans=self.get_annotations(span,base_file)
+        index = Layer(annotation_type=LayerEnum.index,annotations=annotations)
+        opf._index = index
+        opf.save_index()
+        instance = self.add_instances(spans,target_path)
+        return instance """
 
-def create_index(span,target_path,base_file):
-    opf = OpenPechaFS(path=target_path)
-    annotations,spans=get_annotations(span,base_file)
-    index = Layer(annotation_type=LayerEnum.index,annotations=annotations)
-    opf._index = index
-    opf.save_index()
-    instance = add_instances(spans,target_path,base_file)
-    return instance
-
-def get_annotations(span,base_file):
-    page_annotations = {}
-    spans = {}
-    start,end = span
-    spans.update({uuid4().hex:{
-        "base":base_file.stem,
-        "start":start,
-        "end":end
-    }})
-    page_annotaion = {uuid4().hex:{
-        "work":"chojuk",
-        "work_id":get_work_id(),
-        "spans":spans
-    }
-    }
-    page_annotations.update(page_annotaion)
-    return page_annotations,spans
-
-
-def get_matched_span(match):
-    first_match,last_match = match
-    return (first_match.start,last_match.end)
-
-def create_work(instances):
-    works = {
-        "id":uuid4().hex,
-        "title":"chojuk",
-        "instances":instances
-    }
-    works_yml = toyaml(works)
-    if exists("./find_chojuk/works.yml"):
-        works_yml = from_yaml(Path("./find_chojuk/works.yml"))
-        new_instances = [works_yml["instances"],instances]
-        works_yml["instances"]=new_instances
-        works_yml = toyaml(works_yml)
-
-    Path("./find_chojuk/works.yml").write_text(works_yml)
-
-def toyaml(dict):
-    return yaml.safe_dump(dict, sort_keys=False, allow_unicode=True)
-
-def from_yaml(yml_path):
-    return yaml.safe_load(yml_path.read_text(encoding="utf-8"))
-
-def add_instances(spans,target_path,base_file):
-    instance = {
-        uuid4().hex:{
-            "pecha_id":Path(target_path).stem,
+    def get_annotations(self,span,target_file):
+        page_annotations = {}
+        spans = {}
+        start,end = span
+        spans.update({uuid4().hex:{
+            "base":target_file.stem,
+            "start":start,
+            "end":end
+        }})
+        page_annotaion = {uuid4().hex:{
+            "work":"chojuk",
+            "work_id":self.get_work_id(),
             "spans":spans
         }
+        }
+        page_annotations.update(page_annotaion)
+        return page_annotations,spans
 
-    }
-    return instance
 
-def filter_matches(matches):
-    matches_list = []
-    for match in matches:
-        matches_list.append(match[-1])
+    def get_matched_span(self,match):
+        first_match,last_match = match
+        return (first_match.start,last_match.end)
 
-    return tuple(matches_list)    
+    def create_work(self,instances):
+        work = {
+            "id":uuid4().hex,
+            "title":"chojuk",
+            "instances":instances
+        }
+
+        return work
+    
+    @staticmethod
+    def toyaml(dict):
+        return yaml.safe_dump(dict, sort_keys=False, allow_unicode=True)
+
+    @staticmethod
+    def from_yaml(yml_path):
+        return yaml.safe_load(yml_path.read_text(encoding="utf-8"))
+
+    @staticmethod
+    def get_work_id():
+        return "W" + "".join(random.choices(uuid4().hex, k=8)).upper()
+
+    def add_instances(self,spans):
+        instance = {
+            uuid4().hex:{
+                "pecha_id":Path(self.target_path).stem,
+                "spans":spans
+            }
+        }
+        return instance
+
+    def filter_matches(self,matches):
+        matches_list = []
+        for match in matches:
+            matches_list.append(match[-1])
+        return tuple(matches_list) 
+
+
+    def search_sequence(self):
+        instances = []
+        segments = self.get_search_segments()
+        for target_file in self.get_target_files():
+            match = self.fuzzy_search(target_file,segments)
+            if match:
+                print(f"match at {target_file}")
+                match = self.filter_matches(match)
+                span = self.get_matched_span(match)
+                instance = self.create_index(span,target_file)
+                instances.append(instance)
+        work = self.create_work(instance)
+        return work
+
 
 if __name__ == "__main__":
-    search_path = "./find_chojuk/O96FB467A/O96FB467A/O96FB467A.opf/base/03EC.txt"
-    target_path = "./find_chojuk/P000009/P000009.opf"
-    segments = get_search_segments(search_path)
-    time_past = time.time()
-    instances = []
-    for base_file in get_target_files(target_path):
-        print(base_file)
-        match = fuzzy_search(base_file,segments)
-        if match:
-            match = filter_matches(match)
-            span = get_matched_span(match)
-            instance = create_index(span,target_path,base_file)
-            instances.append(instance)
-    create_work(instance)
-
-    print(time.time()-time_past)
-
+    search_path = "./opfs/O96FB467A/O96FB467A/O96FB467A.opf/base/03EC.txt"
+    target_opf_path = "./opfs/P000791"
+    obj = OpfSearchSequence(search_path,target_opf_path)
+    work = obj.search_sequence()
+    print(work)
 
 #module to handle if there is more than one match in first,mid,last
