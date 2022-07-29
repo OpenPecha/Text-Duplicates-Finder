a402ebfdc82f3f87978156eb4983dafcea8c0ae2
Anuj Kumar SHarma <tenzintopjor27@gmail.com>
2022-07-29 10:14:08 +0530

    Extracted most matched files from docfetcher

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,29 @@
-from search import main
 from pathlib import Path
+import search
+import pandas as pd
+
 
 def main():
-    search_text = Path("O96FB467A\O96FB467A\O96FB467A.opf\base\03EC.txt").read_text()
-    
+    dirs = []
+    search_text = Path(r"C:\Users\ASUS\Desktop\z\Text-Duplicates-Finder\O96FB467A\O96FB467A\O96FB467A.opf\base\sample_base.txt").read_text(encoding='utf-8')
+    text = search_text.replace("\n","")
+    chunks, chunk_size = len(text), 50
+    splitted_chunks = [ text[i:i+chunk_size] for i in range(0, chunks, chunk_size) ]
+    for chunk in splitted_chunks:
+        dir = search.main(chunk)
+        dirs.extend(dir)
+
+    most_matched = filter_most_matched(dirs,splitted_chunks)
+    print(most_matched)
+
+def filter_most_matched(dirs,splitted_chunks):
+    most_matched = []
+    counts = pd.Series(dirs).value_counts(ascending=False)
+    for dir,count in counts.items():
+        if count/len(splitted_chunks) > 0.7:
+            most_matched.append(dir)
+    return most_matched
+
+
+if __name__ == "__main__":
+    main()
--- search.py
+++ search.py
@@ -1,59 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
-"""
-This script sends the given command-line arguments as a query to the running
-DocFetcher instance. The results returned by the latter are printed as filename-
-filepath pairs on the standard output.
-
-For more advanced processing of the results, call the search function below
-directly. In principle, you can also reuse the code in the search function for
-arbitrarily scripting the DocFetcher instance.
-
-By default, DocFetcher's scripting support is disabled due to security reasons
-and must be enabled by setting the variable "PythonApiEnabled" in the advanced
-settings file (program-conf.txt) to "true".
-
-Running this script requires Py4J (https://www.py4j.org/). DocFetcher already
-ships with a Py4J distribution, but it only works if the py4j folder is in the
-same folder as this script. To script DocFetcher from a different location, move
-the py4j folder there, or install Py4J separately.
-
-Note that only the main DocFetcher program instance supports scripting, not the
-DocFetcher daemon.
-"""
-
-def main(query):
-	
-	try:
-		result_docs = search(query, 28834)
-		for doc in result_docs:
-			print(doc.getFilename() + "\t" + doc.getPathStr())
-	except:
-		print("ERROR: ")
-
-# string, int -> [ResultDocument]
 def search(query, port):
-	"""Sends the given query string to the running DocFetcher instance at the
-	given port and returns a list of result objects.
-	
-	The result objects provide the following getter methods for accessing their
-	attributes:
-	- getAuthors
-	- getDateStr - e-mail send date
-	- getFilename
-	- getLastModifiedStr - last-modified date on files
-	- getPathStr - file path
-	- getScore - result score as int
-	- getSender - e-mail sender
-	- getSizeInKB - file size as int
-	- getTitle
-	- getType
-	- isEmail - boolean indicating whether result object is e-mail or file
-	
-	This method will throw an error if communication with the DocFetcher
-	instance fails.
-	"""
 	from py4j.java_gateway import JavaGateway, GatewayParameters
 	from py4j.java_gateway import java_import
 	
@@ -65,6 +11,29 @@ def search(query, port):
 	searcher = indexRegistry.getSearcher()
 	results = searcher.search(query)
 	return results
+
+""" def main(query):
+	dirs = []
+	try:
+		result_docs = search(query, 28834)
+		for doc in result_docs:
+			dirs.append(doc.getPathStr())
+	except:
+		print("ERROR: ")
+
+    return dirs """
+
+def main(query):
+	dirs = []
+	try:
+		docs =search(query, 28834)
+		for doc in docs:
+			dirs.append(doc.getFilename())
+	except:
+		print("ERROR: ")
+		
+	return dirs			
+
 
 if __name__ == "__main__":
 	main("བྱང་ཆུབ་སེམས་དཔའི་སྤྱོད་པ་ལ་འཇུག་པ།")
